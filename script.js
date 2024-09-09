5cec6b0e32271adb89dec34e5d90af59f467523b
stephansummers <136759708+stephansummers@users.noreply.github.com>
2024-09-09 11:05:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,13 +19,14 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
 
-        tl.from("#heroText", { opacity: 0, y: 50, duration: 30, delay: 30 }) // Hero Text appears
-            .from("#additionalText1", { opacity: 0, y: 50, duration: 30, delay: 30 }) // Text 1 appears
-            .to("#additionalText1", { opacity: 0, y: -50, duration: 10 }) // Text 1 disappears
-            .from("#additionalText2", { opacity: 0, y: 50, duration: 30 }) // Text 2 appears after Text 1 disappears
-            .from("#additionalText3", { opacity: 0, y: 50, duration: 30 }) // Text 3 appears
-            .to("#additionalText3", { opacity: 0, y: -50, duration: 10, delay: 30 }) // Text 3 disappears
-            .to("#additionalText2", { opacity: 0, y: -50, duration: 10 }) // Text 2 disappears
+        // Sequentially animate texts
+        tl.from("#heroText", { opacity: 0, y: 50, duration: 30, delay: 20 }) // Hero Text appears
+            .from("#additionalText1", { opacity: 0, y: 50, duration: 30, delay: 20 }) // Text 1 appears
+            .to("#additionalText1", { opacity: 0, y: -50, duration: 30 }) // Text 1 disappears
+            .from("#additionalText2", { opacity: 0, y: 50, duration: 30, delay: 20 }) // Text 2 appears after Text 1 disappears
+            .from("#additionalText3", { opacity: 0, y: 50, duration: 30, delay: 20 }) // Text 3 appears
+            .to("#additionalText3", { opacity: 0, y: -50, duration: 30, delay: 30 }) // Text 3 disappears
+            .to("#additionalText2", { opacity: 0, y: -50, duration: 30 }) // Text 2 disappears
             .to("#heroText", { opacity: 0, y: -50, duration: 10 }) // heroText disappears
     
             // Show additionalText4 and CTA button at the same time
